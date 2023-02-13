47b847bdee25e6a040fe493729d72fe1248670b9
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2023-02-13 19:06:17 +0600

    simple update

--- Components/Home/HelloSection.js
+++ Components/Home/HelloSection.js
@@ -1,23 +1,22 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 
 const HelloSection = () => {
   return (
-    <div className="h-auto md:h-screen w-screen flex flex-col md:flex-row">
+    <div className="h-auto md:h-screen w-screen flex flex-col md:flex-row justify-center">
       <div className="w-full md:w-1/2 h-full">
-        <Image
-          src="/4+dorm.jpg"
-          height={0}
-          width={0}
+        <img
+          src="https://i.ibb.co/Kyx4Zps/image.png"
           sizes="100vw"
           className="w-full h-full object-cover"
+          alt=""
         />
       </div>
       <div className="w-full md:w-1/2 h-full p-16 flex flex-col justify-center">
         <h1 className=" text-orange-500 text-7xl italic font-bold">Hello!</h1>
         <p className="text-gray-500 font-semibold py-5 mb-6">WELCOME THERE!</p>
         <p className="text-gray-500 md:w-3/4">
-          Our hostel is located in the city's finest and trendiest district.
+          Our hostel is located in the citys finest and trendiest district.
           Everyone who is interested in being a part of the creative and
           cutting-edge world that is New York, you are welcome to CityHostel for
           the best experience.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from "@next/font/google";
+import Head from "next/head";
 import MainHome from "../Components/Home/MainHome/MainHome";
 const inter = Inter({ subsets: ["latin"] });
 
