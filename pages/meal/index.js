4a78f913740de9a4e19eac34c17e9f82f242bd14
Christos Uster <christos.uster@gmail.com>
2023-02-15 23:30:51 +0600

    temp commit

--- pages/meal/index.js
+++ pages/meal/index.js
@@ -1,17 +1,119 @@
+import { useEffect, useState } from "react";
+import Meal from "../../Components/Meal/Meal";
+import { mealData } from "../../data/meal-data";
+
 export default function Meals() {
+  const [breakfast, setBreakfast] = useState();
+  const [lunch, setLunch] = useState();
+  const [dinner, setDinner] = useState();
+
+  const handleClick = (id, type, typeName, itemPack, price) => {
+    if (type === "breakfast") {
+      setBreakfast({ id, price, itemPack });
+    }
+    if (type === "lunch") {
+      setLunch({ id, price, itemPack });
+    }
+    if (type === "dinner") {
+      setDinner({ id, price, itemPack });
+    }
+  };
+
   return (
     <div className="py-10">
-      <div className="relative h-40 bg-fixed bg-[url('https://i.ibb.co/F0WC8Rv/image.png')] bg-no-repeat bg-cover bg-center mb-20">
+      <div className="relative h-40 bg-fixed bg-[url('https://i.ibb.co/F0WC8Rv/image.png')] bg-no-repeat bg-cover bg-center ">
         <div className="bg-gray-800 h-full w-full opacity-80 "></div>
         <h1 className="absolute left-1/2 top-1/2 text-4xl font-bold text-white -translate-x-1/2 -translate-y-1/2">
           Meal Plan
         </h1>
       </div>
-      <div>
-        <div>
-          <h1></h1>
-          <div></div>
+      <div className="text-center w-2/3 md:w-1/2 max-w-lg mx-auto my-10">
+        <h1 className="text-2xl">Current Plan</h1>
+        <div className="flex justify-between items-center my-3">
+          <div>
+            <h1 className="text-2xl text-orange-300 font-bold">Breakfast:</h1>
+          </div>
+
+          {breakfast ? (
+            <div>
+              <h1 className="text-xs text-gray-400 leading-3">
+                Package {breakfast.itemPack}
+              </h1>
+              <h1 className="text-xl text-gray-400">
+                Tk {breakfast.price} / week
+              </h1>
+            </div>
+          ) : (
+            <h1 className="text-lg text-red-500">Please select one!</h1>
+          )}
         </div>
+
+        <div className="flex justify-between items-center my-3">
+          <div>
+            <h1 className="text-2xl text-orange-300 font-bold">Lunch:</h1>
+          </div>
+
+          {lunch ? (
+            <div>
+              <h1 className="text-xs text-gray-400 leading-3">
+                Package {lunch.itemPack}
+              </h1>
+              <h1 className="text-xl text-gray-400">Tk {lunch.price} / week</h1>
+            </div>
+          ) : (
+            <h1 className="text-lg text-red-500">Please select one!</h1>
+          )}
+        </div>
+
+        <div className="flex justify-between items-center my-3">
+          <div>
+            <h1 className="text-2xl text-orange-300 font-bold">Dinner:</h1>
+          </div>
+
+          {dinner ? (
+            <div>
+              <h1 className="text-xs text-gray-400 leading-3">
+                Package {dinner.itemPack}
+              </h1>
+              <h1 className="text-xl text-gray-400">
+                Tk {dinner.price} / week
+              </h1>
+            </div>
+          ) : (
+            <h1 className="text-lg text-red-500">Please select one!</h1>
+          )}
+        </div>
+        <div className="flex border-t-2 justify-between text-2xl font-bold">
+          <h1>Total:</h1>
+          <h1>Tk {breakfast?.price + lunch?.price + dinner?.price} / week</h1>
+        </div>
+      </div>
+      <div className="flex flex-col items-center text-center">
+        {mealData.map((meal) => {
+          return (
+            <div key={meal.type}>
+              <h1 className="text-4xl font-bold text-orange-500 mb-8 mt-20">
+                {meal.typeName}
+              </h1>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+                {meal.menu.map((item) => {
+                  return (
+                    <Meal
+                      key={item.id}
+                      items={item.items}
+                      price={item.price}
+                      itemPack={item.package}
+                      handleClick={handleClick}
+                      id={item.id}
+                      type={meal.type}
+                      typeName={meal.typeName}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
